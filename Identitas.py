c49795d0abeb2e5debbffa05846fedf62c6cb1ee
Rae <reeyhanmaulana.a.m@gmail.com>
2026-08-23 18:40:17 +0700

    Add files via upload

---
+++ Identitas.py
@@ -0,0 +1,8 @@
+# Pertemuan_1 21 Agustus 2026
+
+print("Halo Semuanya! Perkenalkan Saya\nNama:\tReyhan Maulana A.M");
+print("Nim:\t2605060031")
+print("TTL:\tTemanggung, 15 April 2008")
+print("JK:\tLaki-Laki")
+print("Gaming & \"3D Maker\"")
+#The first off all i learn in basic coding is print and the Magic Happens :-D
