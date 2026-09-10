bb84921aa8624b3b7d434e54247d708130f2af74
Rae <reeyhanmaulana.a.m@gmail.com>
2026-09-10 19:06:40 +0700

    Add files via upload

---
+++ 0031_Reyhan_Operasi_Aritmatika_dan_Komperasi.py
@@ -0,0 +1,19 @@
+#Pertemuan_3 4 September 2026
+
+panjang = 12
+lebar = 5
+tinggi = 8 
+
+print("Soal no 1")
+luas = panjang * lebar
+print("luas =", panjang, "x", lebar, "=", luas)
+volume = panjang * lebar * tinggi
+print("volume =", panjang, "x", lebar, "x", tinggi, "=", volume)
+keliling = 2 * (panjang + lebar)
+print("keliling =", 2, "x", (panjang + lebar), "=", keliling)
+
+print("Soal nomor 2")
+print("Apakah luas lebih dari 50?", luas > 50)
+
+print("Soal nomor 3")
+print("Apakah volume bernilai 480?", volume == 480)
